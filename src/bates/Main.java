20656b89cc9a510a38192106922c55406efd93e4
Angelie Canete Taketomi <71359811+Angelie18@users.noreply.github.com>
2022-01-28 15:32:12 +0900

    Added a response message

--- src/bates/Main.java
+++ src/bates/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Matthew");
+        
+        System.out.println("Hello Matthew, my name is Angelie.");
     }
 }
